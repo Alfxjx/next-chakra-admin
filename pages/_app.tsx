d9d39e0aa4d4334ce47ac008eda3854286aa93f4
xujianxiang <xjxtju@163.com>
2022-02-28 11:01:37 +0800

    feat: add deps & auth base template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return (
+		<ChakraProvider>
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</ChakraProvider>
+	);
 }
-export default MyApp
+export default MyApp;
